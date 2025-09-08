533ea7712c13b5fa7424948e36167845e4e64ec4
Vishnu-Selvaraj <vishnusraj1997@gmail.com>
2025-09-08 09:24:54 +0530

    Some changes

--- online_tour_booking_backend/settings.py
+++ online_tour_booking_backend/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
