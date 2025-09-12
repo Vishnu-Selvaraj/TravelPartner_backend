b1bbd3a82aa26d701b64b1fc528bbf24f168d17b
Vishnu-Selvaraj <vishnusraj1997@gmail.com>
2025-09-12 19:02:09 +0530

    Done some changes

--- online_tour_booking_backend/settings.py
+++ online_tour_booking_backend/settings.py
@@ -31,15 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'admin_app',
     'user_api',
 
     'rest_framework',
     'rest_framework.authtoken',
-
-    'cloudinary',
-    'cloudinary_storage',
 ]
 
 AUTH_USER_MODEL = 'user_api.User'
@@ -60,14 +59,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     "https://travelpartner-yw9z.onrender.com"
 ]
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY':  env('API_KEY'),
-    'API_SECRET': env('API_SECRET'),
-}
 
 ROOT_URLCONF = 'online_tour_booking_backend.urls'
 
@@ -152,7 +143,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#CLOUDINARY CONFIG
+if DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+else:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY':  env('API_KEY'),
+    'API_SECRET': env('API_SECRET'),
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
