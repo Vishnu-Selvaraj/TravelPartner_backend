e743d7b6a07a2ea12240d31c49ed6d54f1163d76
Vishnu-Selvaraj <vishnusraj1997@gmail.com>
2025-09-12 19:09:06 +0530

    changes

--- online_tour_booking_backend/settings.py
+++ online_tour_booking_backend/settings.py
@@ -31,9 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'admin_app',
     'user_api',
 
